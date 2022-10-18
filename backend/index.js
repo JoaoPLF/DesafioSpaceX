9fd6f261704ddcfacaa97c4f8a7d06c317773430
John <joaoplfonseca@hotmail.com>
2022-10-18 19:59:06 -0400

    Adiciona scripts para uso do Google Optimization e muda porta do servidor para deploy no Heroku

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -6,11 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/build')));
 
 const launches = require("./src/routes/launch.route");
 
 app.use("/", launches);
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server running on port ${process.env.SERVER_PORT}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
